2dab9f8a76ced3a4bd36c71e0731323333a903a1
currentapa22 <currentapa22@gmail.com>
2022-11-19 19:12:46 -0600

    last one

--- src/test/java/com/eurotech/utilities/Driver.java
+++ src/test/java/com/eurotech/utilities/Driver.java
@@ -65,6 +65,28 @@ public class Driver {
                     WebDriverManager.getInstance(SafariDriver.class).setup();
                     driverPool.set(new SafariDriver());
                     break;
+                case "remote_chrome":
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    chromeOptions.setCapability("platform",Platform.ANY);
+
+                    try{
+                        driverPool.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),chromeOptions));
+                    }catch (MalformedURLException e){
+                        e.printStackTrace();
+                    }
+
+                    break;
+                case "remote_firefox":
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.setCapability("platform",Platform.ANY);
+
+                    try{
+                        driverPool.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),firefoxOptions));
+                    }catch (MalformedURLException e){
+                        e.printStackTrace();
+                    }
+                    break;
+
             }
         }
         return driverPool.get();
